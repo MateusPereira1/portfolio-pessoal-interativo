3e9885ae101af9a35bb5798bd8740049ca918b57
Mateus Pereira <m.teuspereira09@gmail.com>
2025-09-17 19:02:44 -0300

    :sparkles: feat: creating routes of pages

---
+++ src/app/projects/page.js
@@ -0,0 +1,10 @@
+import Header from "../../../components/header/Header";
+
+export default function Home() {
+  return (
+    <div className="bg-black">
+      <Header/>
+      <h1>Página de projects</h1>
+    </div>
+  );
+}
