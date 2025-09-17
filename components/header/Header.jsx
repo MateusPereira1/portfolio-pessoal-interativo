184cefa82f3044142932fb1e9fdd92925f62d552
Mateus Pereira <m.teuspereira09@gmail.com>
2025-09-17 18:50:37 -0300

    :sparkles: feat: creating header

---
+++ components/header/Header.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+export default function Header() {
+    return (
+        <div className="bgBlue p-4 block flex-row justify-center md:px-48 sm:flex ">
+            <div className="flex justify-center md:justify-start items-center gap-4 w-full flex-row">
+                <Image
+                    alt="Logo Mateus"
+                    src="/vercel.svg"
+                    width={50}
+                    height={50}
+                />
+                <h1 className="text-white text-xl font-bold">Mateus Pereira</h1>
+            </div>
+
+            <nav className="flex flex-row gap-4 mt-2 w-full justify-center md:justify-end">
+                <Link href="/" className="hover:text-gray-400 font-bold text-base  ">Início</Link>
+                <Link href="/about" className="hover:text-gray-400 font-bold text-base  ">Sobre</Link>
+                <Link href="/projects" className="hover:text-gray-400 font-bold text-base  ">Projetos</Link>
+                <Link href="/contact" className="hover:text-gray-400 font-bold text-base  ">Contato</Link>
+            </nav>
+        </div>
+    );
+}
