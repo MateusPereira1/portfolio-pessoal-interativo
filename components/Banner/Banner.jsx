54041467ffe4bd6050a07811f586df41aef550a0
Mateus Pereira <m.teuspereira09@gmail.com>
2025-09-17 18:56:24 -0300

    :sparkles: feat: adding Banner

---
+++ components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+
+
+export default function Banner() {
+    return (
+        <div className="h-180 px-4 banner text-black flex flex-col alight-center justify-center items-center gap-4">
+
+
+            <h1 className="md:text-6xl font-bold text-center text-white text-100 uppercase w-full text-4xl md:w-2/3">Olá, sou Mateus, desenvolvedor front-end</h1>
+            <p className="text-center font-light text-2xl w-full md:w-1/2 text-white">Crio portfólios e interfaces interativas com React e Tailwind</p>
+
+        <Link
+        href="/about"
+        className="bgBlue text-white font-bold  text-lg  rounded-lg px-16 py-2 mt-5 hover:px-23 transition-all "
+        >Ver Projetos</Link>
+            
+
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Header from "../../components/header/Header";
+import Banner from "../../components/Banner/Banner";
 
 export default function Home() {
   return (
     <div className="bg-black">
       <Header/>
+      <Banner/>
     </div>
   );
 }
