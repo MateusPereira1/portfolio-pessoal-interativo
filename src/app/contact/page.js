19b275d8e21638e90a9539744a2074fc49452349
Mateus Pereira <m.teuspereira09@gmail.com>
2025-09-17 23:24:38 -0300

    updating pages with new footer component

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,10 +1,11 @@
 import Header from "../../../components/header/Header";
-
+import Footer from "../../../components/Footer/footer";
 export default function Home() {
   return (
     <div className="bg-black">
       <Header/>
       <h1>Página de contact</h1>
+      <Footer/>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Header from "../../components/header/Header";
 import Banner from "../../components/Banner/Banner";
+import Footer from "../../components/Footer/footer";
 
 export default function Home() {
   return (
     <div className="bg-black">
-      <Header/>
-      <Banner/>
+      <Header />
+      <Banner />
+      <Footer/>
     </div>
   );
 }
