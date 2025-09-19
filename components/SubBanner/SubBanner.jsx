6140edcdb102cb1e909dbbe48e1b292c34dadd36
Mateus Pereira <m.teuspereira09@gmail.com>
2025-09-19 11:22:12 -0300

    :sparkles: feat: creating SubBanner component

---
+++ components/SubBanner/SubBanner.jsx
@@ -0,0 +1,11 @@
+export default function SubBanner(props) {
+    return (
+
+        <div className="banner w-full h-45 items-center flex justify-center">
+            <h1 className="font-xl uppercase text-3xl   font-bold text-center">{props.title}</h1>
+        </div>
+
+
+    )
+
+}
