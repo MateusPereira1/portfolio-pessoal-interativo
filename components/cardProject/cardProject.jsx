77596255eddb0b20a96440f1425fcd004f2c48b7
Mateus Pereira <m.teuspereira09@gmail.com>
2025-09-20 01:45:31 -0300

    :pencil: style: updanting project card style

--- components/cardProject/cardProject.jsx
+++ components/cardProject/cardProject.jsx
@@ -9,26 +9,30 @@ export default function CardProject(props) {
     // desc = descrição da imagem
 
     return (
-        <div className="w-full gap-6 h-80 bgGray rounded-xl ">
+        <div className="w-full gap-6 h-100 bgGray  rounded-xl shadow-2xl  justify-center  pb-3 flex flex-col  ">
 
-            <div className="w-full h-1/2 relative rounded-t-xl overflow-hidden">
+            <div className="w-full h-[180px] relative rounded-t-xl overflow-hidden">
                 <Image
                     src={props.src}
                     alt={props.title}
                     fill
-                    style={{ objectFit: "cover", objectPosition: "center" }}
+                    style={{ objectFit: "cover", objectPosition: "top" }}
                 />
             </div>
 
 
-            <div className="w-full h-1/2 p-3 ">
+            <div className="w-full h-[120px] p-3 ">
 
 
-                <h1 className="text-black font-bold text-2xl">{props.title}</h1>
+                <h1 className="text-black font-bold text-3xl">{props.title}</h1>
 
-                <p className="text-black text-l font-normal">{props.desc}</p>
+                <p className="text-black text-xl font-normal">{props.desc}</p>
 
             </div>
+
+            <a href={props.link} target="_blank" className="bgBlue hover:cursor-pointer text-white text-lg w-1/2 text-center mx-auto rounded-xl uppercase font-bold py-3 hover:w-2/3 transition-all"> Ver Mais</a>
+
+
         </div>
     )
 
